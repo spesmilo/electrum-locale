3c251bb07f5498cae6856534faedeb0eb39ac644
SomberNight <somber.night@protonmail.com>
2025-04-03 18:48:34 +0000

    fix update.py: crowdin removed unauthenticated API
    see https://github.com/spesmilo/electrum/issues/9531

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import os
 import sys
 import io
@@ -11,14 +12,66 @@ except ImportError as e:
     sys.exit(f"Error: {str(e)}. Try 'python3 -m pip install --user <module-name>'")
 
 
-def pull_locale(path):
+crowdin_project_id = 20482  # for "Electrum" project on crowdin
+
+
+def get_crowdin_api_key() -> str:
+    crowdin_api_key = None
+    if "crowdin_api_key" in os.environ:
+        return os.environ["crowdin_api_key"]
+    filename = os.path.expanduser('~/.crowdin_api_key')
+    if os.path.exists(filename):
+        with open(filename) as f:
+            crowdin_api_key = f.read().strip()
+    return crowdin_api_key
+
+
+def pull_locale(path, *, crowdin_api_key=None):
+    global_headers = {}
+    if crowdin_api_key is None:
+        crowdin_api_key = get_crowdin_api_key()
+    if not crowdin_api_key:
+        # Looks like crowdin does not even allow downloading without auth anymore.
+        raise Exception("missing required crowdin_api_key")
+    if crowdin_api_key:
+        global_headers["Authorization"] = "Bearer {}".format(crowdin_api_key)
+
     if not os.path.exists(path):
         os.mkdir(path)
     os.chdir(path)
 
+    # note: We won't request a build now, instead we download the latest build.
+    #       This assumes that the push_locale script was run recently (in the past few days).
+    print('Getting list of builds from crowdin...')
+    # https://support.crowdin.com/developer/api/v2/?q=api#tag/Translations/operation/api.projects.translations.builds.getMany
+    url = f'https://api.crowdin.com/api/v2/projects/{crowdin_project_id}/translations/builds'
+    headers = {**global_headers, **{"content-type": "application/json"}}
+    response = requests.request("GET", url, headers=headers)
+    response.raise_for_status()
+    print("", "translations.builds.getMany:", "-" * 20, response.text, "-" * 20, sep="\n")
+
+    latest_build = response.json()["data"][0]["data"]
+    assert latest_build["status"] == "finished", latest_build["status"]
+    created_at = datetime.datetime.fromisoformat(latest_build["createdAt"])
+    if (datetime.datetime.now(datetime.timezone.utc) - created_at) > datetime.timedelta(days=2):
+        raise Exception(f"latest translation build looks too old. {created_at.isoformat()=}")
+    build_id = latest_build["id"]
+
+    print('Asking crowdin to generate a URL for the latest build...')
+    # https://support.crowdin.com/developer/api/v2/?q=api#tag/Translations/operation/api.projects.translations.builds.download.download
+    url = f'https://api.crowdin.com/api/v2/projects/{crowdin_project_id}/translations/builds/{build_id}/download'
+    headers = {**global_headers, **{"content-type": "application/json"}}
+    response = requests.request("GET", url, headers=headers)
+    response.raise_for_status()
+    print("", "translations.builds.download.download:", "-" * 20, response.text, "-" * 20, sep="\n")
+
+    build_url = response.json()["data"]["url"]
+
     # Download & unzip
     print('Downloading translations...')
-    s = requests.request('GET', 'https://crowdin.com/backend/download/project/electrum.zip').content
+    response = requests.request('GET', build_url, headers={})
+    response.raise_for_status()
+    s = response.content
     zfobj = zipfile.ZipFile(io.BytesIO(s))
 
     print('Unzipping translations...')
@@ -38,6 +91,11 @@ if __name__ == '__main__':
     path_here = os.path.dirname(os.path.realpath(__file__))
     path_locale = os.path.join(path_here, "locale")
     pull_locale(path_locale)
+
+    print('Local updates done.')
+    c = input("Do you want to git commit this? (y/n): ")
+    if c != "y":
+        sys.exit(0)
 
     print('Preparing git commit...')
     os.chdir(path_here)
